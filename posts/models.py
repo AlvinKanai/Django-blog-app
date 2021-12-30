4098703e4857507e26de37c9ad1d1e8c7e4878eb
AlvinKanai <alvinkanai@gmail,com>
2021-12-30 03:54:57 +0300

    blog post app init and loaded sample blogs

---
+++ posts/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+# Create your models here.
+User = get_user_model()
+
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField()
+
+    def __str__(self):
+        return self.user.username
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    overview = models.TextField()
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    comment_count = models.IntegerField(default=0)
+    thumbnail = models.ImageField()
+    categories = models.ManyToManyField(Category)
+    featured = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
